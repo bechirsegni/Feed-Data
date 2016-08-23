ef2b625edaef00eedb5839d151509c8b99506616
Bechir Segni <bechirsegni@gmail.com>
2016-08-23 23:30:45 +0100

    Fixing The number of data in the chart to 10

--- web/main.js
+++ web/main.js
@@ -10,19 +10,30 @@ $.getJSON("vox.json", function(json) {
   });
 
 var lineChart = new Chart(CHART, {
-    type: 'bar',
+    type: 'line',
     data: data = {
-        labels: keys,
+        labels: keys.slice(0,9),
         datasets: [
             {
                 label: "Words frequency in Rss Feed",
-                backgroundColor: "#e74c3c",
-                borderColor: "#e74c3c",
-                pointBackgroundColor: "rgba(179,181,198,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(179,181,198,1)",
-                data: values
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: values.slice(0,9)
             }
 
         ]
